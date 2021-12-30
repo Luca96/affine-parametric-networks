85050c8b0f3f8ccc62932a52c2745eee462b59b1
luca96 <luca.anzalone.dev@gmail.com>
2021-12-30 20:36:49 +0100

    updated best-cuts for CAT-2 + new notebook

--- script/cms/__init__.py
+++ script/cms/__init__.py
@@ -4,6 +4,6 @@ from script.cms.data import MassBalancedSequence
 
 from script.cms import plot
 
-from script.cms.utils import get_test_from_dataset, CutBased, IndividualNNs
+from script.cms.utils import get_test_from_dataset, CutBased, CutBased2, IndividualNNs
 from script.cms.utils import STATS_CAT1, STATS_CAT2, CLIP_CAT1, CLIP_CAT2
 from script.cms.utils import retrieve_stat, retrieve_clip
